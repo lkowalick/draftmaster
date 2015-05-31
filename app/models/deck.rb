0fdca023be9a5f553a525306605c2f979ace8594
Ryan Kowalick <rkowalick@gmail.com>
2015-05-31 16:12:10 -0400

    Associate users with decks

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -2,6 +2,7 @@
 class Deck < ActiveRecord::Base
   has_many :card_decks, dependent: :destroy
   has_many :cards, through: :card_decks
+  belongs_to :user
 
   def most_recently_added_set
     card_deck = card_decks.order(created_at: :desc).first
