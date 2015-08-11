ed3853a84bb1e687a5130dd00c8dfd62bda7b1c7
Ryan Kowalick <rkowalick@covermymeds.com>
2015-08-10 23:05:19 -0400

    Actual editing of matches

---
+++ app/controllers/matches_controller.rb
@@ -0,0 +1,21 @@
+class MatchesController < ApplicationController
+  def update
+    @match = Match.find(params[:id])
+
+    if @match.update_attributes(match_params)
+      flash[:notice] = 'Match results entered'
+    else
+      flash[:alert] = 'There was a problem entering match results'
+    end
+    redirect_to @match.round
+  end
+
+  private
+
+  def match_params
+    params.require(:match).permit(:completed,
+                                  :draws,
+                                  :player_one_wins,
+                                  :player_two_wins)
+  end
+end
